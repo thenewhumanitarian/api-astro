15a7187d914e98c05ffdb2d6be11965765cbb45e
Marc Fehr <marc.fehr@gmail.com>
2023-11-26 21:07:58 +0200

    adds ssr vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import prefetch from '@astrojs/prefetch';
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  // output: 'server',
-  // adapter: vercel(),
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind({
     applyBaseStyles: false
   }), prefetch()]
