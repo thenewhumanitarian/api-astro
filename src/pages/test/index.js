e2c8fbe1df536bf86b6aadf171b718bd0e7d9c90
Marc Fehr <marc.fehr@gmail.com>
2023-11-26 21:01:10 +0200

    CORS

--- src/pages/test/index.js
+++ src/pages/test/index.js
@@ -1,8 +1,9 @@
-export const POST = ({ request }) => {
-    console.log(request)
-
-    return new Response(JSON.stringify({
-        message: "This was a POST!"
-    })
-    )
-}
+export const POST = async ({ request }) => {
+    const headers = new Headers({
+      "Access-Control-Allow-Origin": "*", // Allows all origins
+      "Content-Type": "application/json"
+    });
+    
+    // Your existing code
+    return new Response(JSON.stringify({ message: "This was a POST!" }), { headers });
+  };
