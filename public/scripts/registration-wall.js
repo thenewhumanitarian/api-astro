d5bb4650f4c22355d2cef09a15097680acbe1ab3
Marc Fehr <marc.fehr@gmail.com>
2023-12-09 09:29:36 +0200

    adds CSS inline

--- public/scripts/registration-wall.js
+++ public/scripts/registration-wall.js
@@ -114,26 +114,28 @@
         errorMessage.classList.add('error-message');
         form.appendChild(errorMessage);
 
-        var interests = window.interests || ['Aid and Policy', 'Conflict', 'Environment and Disasters', 'Investigations', 'Migration'];
-        interests.forEach(function (interest) {
-            var checkboxContainer = document.createElement('div');
-            checkboxContainer.classList.add('checkbox-container');
+        var interests = window.interests;
+        if (interests.length > 0) {
+            interests.forEach(function (interest) {
+                var checkboxContainer = document.createElement('div');
+                checkboxContainer.classList.add('checkbox-container');
 
-            var checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.id = interest;
-            checkbox.name = 'interests';
-            checkbox.value = interest;
+                var checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.id = interest;
+                checkbox.name = 'interests';
+                checkbox.value = interest;
 
-            var label = document.createElement('label');
-            label.htmlFor = interest;
-            label.textContent = interest;
-            label.classList.add('checkbox-label');
+                var label = document.createElement('label');
+                label.htmlFor = interest;
+                label.textContent = interest;
+                label.classList.add('checkbox-label');
 
-            checkboxContainer.appendChild(checkbox);
-            checkboxContainer.appendChild(label);
-            grid.appendChild(checkboxContainer);
-        });
+                checkboxContainer.appendChild(checkbox);
+                checkboxContainer.appendChild(label);
+                grid.appendChild(checkboxContainer);
+            });
+        }
 
         form.appendChild(submitButton);
 
@@ -265,6 +267,306 @@
     }
     document.body.insertBefore(pageContent, modal);
     pageContent.style.filter = 'blur(5px)'; // blur effect
+
+    // CSS styles
+    var styles = `
+    /* Typography */
+    h2 {
+        margin-top: 0.25rem;
+    }
+
+    #existing-member {
+        display: none;
+    }
+
+    #registration-wall {
+        width: 100%;
+        height: 100vh;
+        overflow: hidden;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+
+    .modal {
+        font-family: 'Roboto', sans-serif;
+        display: block;
+        position: fixed;
+        z-index: 1;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+        background-color: rgba(0, 0, 0, 0.4);
+        backdrop-filter: blur(8px);
+    }
+
+    .modal-content {
+        background-color: #fefefe;
+        margin: 15% auto;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 90%;
+        max-width: 640px;
+    }
+
+    .modal-title {
+        color: #9f3e52;
+        text-align: left;
+        margin-bottom: 0.5rem;
+    }
+
+    .error-message {
+        text-align: center;
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.4rem;
+        padding: 1rem 0;
+    }
+
+    #registration-form input {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.4rem;
+    }
+
+    .interest-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+        column-gap: 1rem;
+        row-gap: 0.5rem;
+    }
+
+    .checkbox-container {
+        margin-bottom: 0.35rem;
+        display: flex;
+        column-gap: 1rem;
+        justify-content: flex-start;
+        align-items: center;
+    }
+
+    /* Form elements */
+
+    .form-label {
+        margin-bottom: 0.25rem;
+    }
+
+    .checkbox-label {
+        margin: 0;
+        font-size: 1.4rem;
+    }
+
+    .interests-label {
+        margin: 1rem 0;
+    }
+
+    input[type=text],
+    input[type=email],
+    select {
+        width: 100%;
+        padding: 0.78rem;
+        display: inline-block;
+        border: 1px solid #ccc;
+        box-sizing: border-box;
+        margin-bottom: 0.75rem;
+        font-size: 1rem;
+    }
+
+    input[type=text]:focus,
+    input[type=email]:focus,
+    input[type=checkbox]:focus {
+        outline: none;
+        border-color: #9f3e52;
+        box-shadow: 0 0 0 2px rgba(159, 62, 82, 0.2);
+    }
+
+    input[type=submit] {
+        width: 100%;
+        font-weight: 600;
+        font-size: 1rem;
+        background-color: #9f3e52;
+        color: white;
+        padding: 14px 20px;
+        margin: 8px 0;
+        border: none;
+        cursor: pointer;
+    }
+
+    input[type=submit]:hover {
+        background-color: #8d3749;
+    }
+
+    #pageContent {
+        transition: filter 0.3s;
+    }
+
+    .toggle-button {
+        color: #9f3e52;
+        display: block;
+        margin-bottom: 1rem;
+    }
+
+    .intro-text {
+        text-align: left;
+        margin-bottom: 20px;
+    }
+
+    /* Tooltips */
+
+    .tooltip {
+        position: relative;
+        display: inline-block;
+        text-decoration-line: underline;
+        text-decoration-style: wavy;
+        text-decoration-color: #5d5d5d;
+        text-decoration-thickness: 1px;
+        font-weight: bold;
+    }
+
+    .tooltip:hover {
+        cursor: help;
+        color: #9f3e52;
+    }
+
+    .field-name-body {
+        position: relative;
+    }
+
+    .side-annotation {
+        padding-top: 1rem;
+        margin-top: 0;
+        position: absolute;
+        left: 105%;
+        width: 100%;
+        max-width: 30rem;
+        border-top: .1rem solid #000;
+        transition: opacity 0.2s ease-in, opacity 0.5s ease-out;
+        /* Add transitions with different timing functions */
+        will-change: opacity;
+    }
+
+    .modal-annotation,
+    .modal-annotation.active {
+        display: none;
+    }
+
+    /* Responsive breakpoints */
+
+    @media screen and (max-width: 1250px) {
+        .side-annotation {
+            width: 25%;
+        }
+    }
+
+    @media screen and (max-width: 1150px) {
+        .side-annotation {
+            width: 20%;
+        }
+    }
+
+    @media screen and (max-width: 1050px) {
+        .side-annotation {
+            display: none;
+        }
+
+        .modal-annotation {
+            display: none;
+            position: fixed;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0, 0, 0, 0.8);
+            justify-content: center;
+            align-items: center;
+        }
+
+        /* Show the modal box when the class 'active' is added */
+        .modal-annotation.active {
+            display: flex;
+        }
+
+        .modal-content {
+            position: relative;
+            background-color: #fff;
+            padding: 2rem;
+            margin: auto 2rem;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+        }
+
+        .modal-close {
+            position: absolute;
+            font-size: 3rem;
+            top: -6px;
+            right: 8px;
+            cursor: pointer;
+        }
+
+        .modal-close:hover {
+            color: red;
+        }
+
+        .modal-content {
+            margin: 0;
+            max-width: unset;
+        }
+    }
+
+    @media screen and (max-width: 500px) {
+        .modal-content {
+            max-width: 100%;
+            margin: 0;
+            overflow: scroll;
+            height: 90vh;
+            min-height: 90vh;
+            min-height: -webkit-fill-available;
+        }
+
+        .interest-grid {
+            grid-template-columns: repeat(auto-fit, minmax(140px, 1fr));
+            column-gap: 0.5rem;
+            row-gap: 0.25rem;
+        }
+
+        .checkbox-container * {
+            font-size: 1.2rem;
+        }
+
+        .title-text {
+            font-size: 1.2rem;
+        }
+
+        .intro-text {
+            font-size: 1rem;
+        }
+
+        .modal-title,
+        .intro-text {
+            text-align: left;
+        }
+
+        #new-registration label {
+            font-size: 0.9rem;
+        }
+
+        #new-registration input:not([type="submit"]) {
+            font-family: 'Roboto', sans-serif;
+            font-size: 1rem;
+            padding: 0.25rem;
+            margin-bottom: 0.5rem;
+        }
+
+        #new-registration input[type="submit"] {
+            margin-top: 0.7rem;
+            padding: 0.6rem;
+        }
+    }
+    `;
+
+    var styleSheet = document.createElement("style");
+    styleSheet.type = "text/css";
+    styleSheet.innerText = styles;
+    document.head.appendChild(styleSheet);
+
 
     /* Helper functions */
 
