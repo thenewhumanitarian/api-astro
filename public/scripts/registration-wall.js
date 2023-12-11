088fe178214ff6c1ea305fcf01bebcd12a1e39d9
Marc Fehr <marc.fehr@gmail.com>
2023-12-11 08:17:21 +0200

    change css import

--- public/scripts/registration-wall.js
+++ public/scripts/registration-wall.js
@@ -275,7 +275,7 @@
     pageContent.style.filter = 'blur(5px)'; // blur effect
 
     console.log(window.location)
-    var styleSheetUrl = window.location.hostname === "localhost" ? "/styles/annotations.css" : "https://api.thenewhumanitarian.org/styles/registration-wall.css";
+    var styleSheetUrl = window.location.hostname === "localhost" ? "/styles/annotations.css" : "https://api.thenewhumanitarian.org/styles/annotations";
 
 
     var link = document.createElement("link");
